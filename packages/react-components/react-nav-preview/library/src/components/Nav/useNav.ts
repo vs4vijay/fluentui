488a17442ce8264281b14a4e66d715c2390001ad
Mason Tejera <matejera@microsoft.com>
2024-09-17 15:11:43 -0700

    more cleanup

--- packages/react-components/react-nav-preview/library/src/components/Nav/useNav.ts
+++ packages/react-components/react-nav-preview/library/src/components/Nav/useNav.ts
@@ -73,7 +73,7 @@ export const useNav_unstable = (props: NavProps, ref: React.Ref<HTMLDivElement>)
   const [openCategories, setOpenCategories] = useControllableState({
     state: controlledOpenCategoryItems,
     defaultState: initializeUncontrolledOpenCategories({ defaultOpenCategories, multiple }),
-    initialState: undefined,
+    initialState: [],
   });
 
   const [selectedCategoryValue, setSelectedCategoryValue] = useControllableState({
@@ -155,9 +155,9 @@ export const useNav_unstable = (props: NavProps, ref: React.Ref<HTMLDivElement>)
       }),
       { elementType: 'div' },
     ),
-    openCategories: openCategories && openCategories.length > 0 ? openCategories : [],
+    openCategories,
     selectedValue,
-    selectedCategoryValue: controlledSelectedCategoryValue ?? selectedCategoryValue,
+    selectedCategoryValue,
     onRegister,
     onUnregister,
     onSelect,
